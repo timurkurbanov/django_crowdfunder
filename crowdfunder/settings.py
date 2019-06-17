f6db8d2cedd3bdc4eeb0c439a10039ba58bc16fb
TIMUR <1tkurbanov@gmail.com>
2019-06-17 16:55:37 -0400

    Update db to postgres

--- crowdfunder/settings.py
+++ crowdfunder/settings.py
@@ -81,20 +81,27 @@ WSGI_APPLICATION = 'crowdfunder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'crowdfunder_dev'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('DB_NAME', ''),
-#         'USER': os.environ.get('DB_USER', ''),
-#         'PASSWORD': os.environ.get('DB_PASS', ''),
-#         'HOST': 'localhost',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'seatyourself_development',
+#         'HOST': '127.0.0.1',
 #         'PORT': '5432',
 #     }
 # }
